ef2b6349915ad68b5b57a29c95c6b0eaa273cb4e
Guillermo Olivos <gcolivos@gmail.com>
2018-03-18 14:44:04 -0500

    firebase tutorial app complete

--- app.js
+++ app.js
@@ -1,26 +1,53 @@
-  require ('dotenv').config();
 
-  // Initialize Firebase
-  var config = {
-    apiKey: process.env.GOOGLE_API_KEY,
-    authDomain: "fir-tutorial-c6aa1.firebaseapp.com",
-    databaseURL: "https://fir-tutorial-c6aa1.firebaseio.com",
-    projectId: "fir-tutorial-c6aa1",
-    storageBucket: "fir-tutorial-c6aa1.appspot.com",
-    messagingSenderId: "155080021447"
-  };
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyA-eU3PwVrRIIwM-f2hVgrz4fLW-L-9FsE",
+  authDomain: "fir-tutorial-c6aa1.firebaseapp.com",
+  databaseURL: "https://fir-tutorial-c6aa1.firebaseio.com",
+  projectId: "fir-tutorial-c6aa1",
+  storageBucket: "fir-tutorial-c6aa1.appspot.com",
+  messagingSenderId: "155080021447"
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  var header = document.getElementById('header');
+var header = document.getElementById('header');
 
 //pointer to database, telling it where to look
-  var dbRef = firebase.database().ref.child('header');
+var dbRef = firebase.database().ref().child('header');
 
 //dbRef.on('value') means whenever value changes in Firebase, this will trigger the following code to run, the function taking snap as a parameter
 //snap is a snapshot of the header node
 //sets header's innerText (the H1) with the data
-dbRef.on('value', function(snap){
-    header.innerText = snap.val();
+
+var signInButton = document.getElementById('signInButton');
+var signOutButton = document.getElementById('signOutButton');
+var provider = new firebase.auth.GoogleAuthProvider();
+
+signInButton.addEventListener('click', function(){
+  firebase.auth().signInWithPopup(provider).then(function(user){
+    if (user){
+      dbRef.on('value', function (snap) {
+        header.innerText = snap.val();
+      });
+
+      playgroundRef.on('value', function (snap) {
+        playground.value = snap.val();
+      });
+    }
+  });
+});
+
+signOutButton.addEventListener('click', function(){
+  firebase.auth().signOut().then(function(){
+    header.innerText = 'Sign In With Google To See The Magic';
+    playground.value = 'You need to sign in to see the playground';
+  })
 })
 
+var playground = document.getElementById('playground');
+var playgroundRef = firebase.database().ref().child('playground');
+
+playground.addEventListener('keyup', function(){
+  playgroundRef.set(playground.value);
+});
